27032e3a2c2407e876c91339e7b6ca17cfe182e5
semdeleer <145379586+semdeleer@users.noreply.github.com>
2024-03-25 16:13:30 +0100

    FastAPI

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from predict.predict import predict
+
+
+app = FastAPI()
+
+class jsonIn(BaseModel):
+    postalcode: int
+    property_type: str 
+    property_subtype: str
+    type_of_sale: str 
+    living_area: float
+    kitchen_type: str 
+    fully_equipped_kitchen: float 
+    open_fire: int
+    terrace: int
+    terrace_area: float
+    garden: float
+    garden_area: float
+    surface_of_good: float
+    number_of_facades: int
+    swimming_pool: float
+    state_of_building: str
+    main_city: str
+    province: str
+
+class PredictionOut(BaseModel):
+    price: float
+
+@app.get("/")
+def home():
+    return {"health_check": "OK"}
+
+@app.post("/predict", response_model=PredictionOut)
+def predict_d(payload: jsonIn):
+    
+    price = predict(payload.postalcode, payload.property_type, payload.property_subtype, payload.type_of_sale, payload.living_area, payload.kitchen_type, payload.fully_equipped_kitchen, payload.open_fire, payload.terrace, payload.terrace_area, payload.garden, payload.garden_area, payload.surface_of_good, payload.number_of_facades, payload.swimming_pool, payload.state_of_building, payload.main_city, payload.province)
+    return {"price": price}
--- predict/predict.py
+++ predict/predict.py
@@ -1,40 +1,14 @@
 import pickle
 import pandas as pd
 
-model = open("model_pickle", "rb")
-model = pickle.load(model)
-dt = open("model_pickle_dt", "rb")
-dt = pickle.load(dt)
-modelb = open("model_pickle_b", "rb")
+modelb = open(r"./predict/model_pickle_b", "rb")
 modelb = pickle.load(modelb)
 
 
-predictlr= [[
-    8500, 920.0, 800.000000, 32.768764, 4, 6, 3.0, 0.0, 1, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+def predict(postalcode: int, property_type: str, property_subtype: str, type_of_sale: str,  living_area: float, kitchen_type: str, fully_equipped_kitchen: float, open_fire: int, terrace: int, terrace_area: float, garden: float, garden_area: float, surface_of_good: float, number_of_facades: int, swimming_pool: float, state_of_building: str, main_city: str, province: str):
+    house_data = {'postal_code' :[postalcode],'property_type' :[property_type],'property_subtype' :[property_subtype],'type_of_sale':[type_of_sale],'living_area':[living_area],'kitchen_type':[kitchen_type],'fully_equipped_kitchen':[fully_equipped_kitchen],'open_fire':[open_fire],'terrace':[terrace],'terrace_area':[terrace_area],'garden':[garden],'garden_area':[garden_area],'surface_of_good':[surface_of_good],'number_of_facades':[number_of_facades],'swimming_pool':[swimming_pool],'state_of_building':[state_of_building],'main_city':[main_city], "province":[province]}
+    test_df = pd.DataFrame(house_data)
+    bpr = modelb.predict(test_df)
+    return bpr
 
-lr = model.predict(predictlr)
-
-
-p = {"price": [240000.0 ], "surface_of_good": [165.0], "swimming_pool": [1.0], "state_of_building": [4]}
-testdata = pd.DataFrame(p)
-
-cl = dt.predict(testdata)
-
-
-house_data = {'postal_code' :[1060],'property_type' :["HOUSE"],'property_subtype' :["HOUSE"],'type_of_sale':["BUY_REGULAR"],'living_area':["220.0"],'kitchen_type':["SEMI_EQUIPPED"],'fully_equipped_kitchen':[1.0],'open_fire':[0],'terrace':[0],'terrace_area':[50.0],'garden':[1.0],'garden_area':[100.0],'surface_of_good':[218.0],'number_of_facades':[4],'swimming_pool':[1.0],'state_of_building':['GOOD'],'main_city':['wevelgem'], "province":["west-vlaanderen"]}
-test_df = pd.DataFrame(house_data)
-
-bpr = modelb.predict(test_df)
-
-print(f"liner regression model:{lr} \n"
-      f"discicion Tree model: {cl}\n"
-      f"bagging regressor model:{bpr}")
+#print(f"bagging regressor model:{predict()}")
