ece0848ddb972e4993c5e289057f29e563b296c2
semdeleer <145379586+semdeleer@users.noreply.github.com>
2024-03-26 11:08:46 +0100

    changed the streamlit with dropdwon boxes

--- streamlit/steamlit.py
+++ streamlit/steamlit.py
@@ -22,11 +22,12 @@ def main():
     # getting the input data from the user
 
     postalcode = st.text_input("postalcode")
-    property_type = st.text_input("property_type")
-    property_subtype = st.text_input("property_subtype")
-    type_of_sale = st.text_input("type_of_sale")
+    property_type = st.selectbox(label = "Choose the type of property",index=None, options=["HOUSE"])
+    property_subtype = st.selectbox(label = "Choose the subtype of property",index=None, options=['HOUSE', 'MIXED_USE_BUILDING', 'APARTMENT_BLOCK', 'BUNGALOW', 'VILLA','EXCEPTIONAL_PROPERTY' ,'COUNTRY_COTTAGE' ,'MANSION', 'FARMHOUSE','TOWN_HOUSE' ,'MANOR_HOUSE', 'CHALET', 'OTHER_PROPERTY' ,'CASTLE'])
+    type_of_sale = st.selectbox(label = "How do you want to sell your house",index=None, options=['BUY_REGULAR' ,'PUBLIC_SALE' ,'LIFE_ANNUITY'])
     living_area = st.text_input("living_area")
-    kitchen_type = st.text_input("kitchen_type")
+    kitchen_type = st.selectbox(label = "What is the kitchen type",index=None, options=['INSTALLED', 'USA_HYPER_EQUIPPED', 'NOT_INSTALLED' ,'HYPER_EQUIPPED',
+ 'SEMI_EQUIPPED' ,'USA_SEMI_EQUIPPED','USA_INSTALLED', 'USA_UNINSTALLED'])
     fully_equipped_kitchen = st.text_input("fully_equipped_kitchen")
     open_fire = st.text_input("open_fire")
     terrace = st.text_input("terrace")
@@ -34,11 +35,62 @@ def main():
     garden = st.text_input("garden")
     garden_area = st.text_input("garden_area")
     surface_of_good = st.text_input("surface_of_good")
-    number_of_facades = st.text_input("number_of_facades")
+    number_of_facades = st.selectbox(label = "How many wall's are free of your house?",index=None, options=[1,2,3,4])
     swimming_pool = st.text_input("swimming_pool")
-    state_of_building = st.text_input("state_of_building")
-    main_city = st.text_input("main_city")
-    province = st.text_input("province")
+    state_of_building = st.selectbox(label = "How is the state of your home?",index=None, options=['GOOD' ,'AS_NEW' ,'TO_BE_DONE_UP', 'TO_RENOVATE','JUST_RENOVATED',
+ 'TO_RESTORE'])
+    main_city = st.selectbox(label = "What is the city the house is located in?",index=None, options=[
+    'ronse', 'merelbeke', 'trooz', 'ottignies-louvain-la-neuve', 'antwerpen',
+    'huy', 'liège', 'marchin', 'oupeye', 'elsene', 'erpe-mere', 'beyne-heusay',
+    'halle', 'rixensart', 'vorst', 'ukkel', 'courcelles', 'châtelet', 'brugge',
+    'poperinge', 'nieuwpoort', 'anderlecht', 'tessenderlo', 'wevelgem', 'kuurne',
+    'schilde', 'nijlen', 'blankenberge', 'geraardsbergen', 'knokke-heist',
+    'oostende', 'spa', 'sint-gillis-waas', 'steenokkerzeel', 'sint-niklaas',
+    'dilbeek', 'berloz', 'wetteren', 'gent', 'seraing', 'bonheiden', 'evergem',
+    'deinze', 'amay', 'affligem', 'kruisem', 'braives', 'charleroi', 'gerpinnes',
+    'amel', 'ittre', 'ham', 'ath', 'wielsbeke', 'aalst', 'mechelen', 'brussel',
+    'messancy', 'vilvoorde', 'villers-le-bouillet', 'burdinne', 'maarkedal',
+    'overijse', 'meise', 'brakel', 'nassogne', 'herstal', "braine-l'alleud",
+    'puurs-sint-amands', 'havelange', 'londerzeel', 'walcourt', 'andenne',
+    'manhay', 'beringen', 'retie', 'lummen', 'grâce-hollogne', 'leuven',
+    'sint-martens-latem', 'ferrières', 'edegem', 'tongeren', 'quaregnon',
+    'turnhout', 'heist-op-den-berg', 'la louvière', 'haaltert',
+    'kapelle-op-den-bos', 'bever', 'visé', 'destelbergen', 'sint-genesius-rode',
+    'mortsel', 'bièvre', 'zottegem', 'oudenburg', 'lessines', 'hannut', 'wemmel',
+    'wezembeek-oppem', 'rochefort', 'machelen', 'koksijde', 'lebbeke', 'assenede',
+    'flémalle', 'kampenhout', 'waterloo', 'sint-pieters-leeuw', 'wachtebeke',
+    'braine-le-château', 'ingelmunster', 'galmaarden', 'la hulpe', 'beersel',
+    'ninove', 'paliseul', 'zwijndrecht', 'eeklo', 'saint-nicolas', 'estaimpuis',
+    'erquelinnes', 'viroinval', 'antoing', 'bertrix', 'genappe', 'lede', 'bredene',
+    'izegem', 'asse', 'chastre', 'la roche-en-ardenne', 'tubize', 'maldegem',
+    'boechout', 'opwijk', 'brasschaat', 'oudenaarde', 'linter', 'avelgem',
+    'tervuren', 'herne', 'duffel', 'dendermonde', 'zaventem', 'neupré', 'harelbeke',
+    'ciney', 'daverdisse', 'court-saint-etienne', 'denderleeuw', 'moeskroen',
+    'scherpenheuvel-zichem', 'tournai', 'ganshoren', 'aarschot', 'willebroek',
+    'boussu', 'rumst', 'houthalen-helchteren', 'walhain', 'kluisbergen',
+    'kraainem', 'herent', 'waregem', 'malmedy', 'wichelen', 'deerlijk',
+    'oud-turnhout', 'kalmthout', 'temse', 'beaumont', 'stabroek',
+    'libramont-chevigny', 'oudergem', 'keerbergen', 'namur', 'kortenberg',
+    'zonhoven', 'pepinster', 'sint-lambrechts-woluwe', 'verviers', 'ravels',
+    'wellin', 'wanze', 'marche-en-famenne', 'wavre', 'niel', 'beveren-waas',
+    'herentals', 'morlanwelz', 'zoersel', 'ternat', 'libin', 'aubange', 'essen',
+    'remicourt', 'bassenge', 'ans', 'wasseiges', 'diksmuide',
+    'sint-agatha-berchem', 'dentergem', 'zwevegem', 'hamme', 'kortrijk',
+    'schaarbeek', 'tienen', 'frasnes-lez-anvaing', 'zedelgem', 'erezée',
+    'roeselare', 'tellin', 'heers', 'menen', 'villers-la-ville', 'haacht',
+    'gembloux', 'landen', 'blégny', 'lier', 'geer', 'boortmeerbeek', 'putte',
+    'berlaar', 'rendeux', 'durbuy', 'hooglede', 'grimbergen', 'bertem', 'brunehaut',
+    'ellezelles', 'saint-hubert', 'aiseau-presles', 'kruibeke', 'torhout',
+    'leopoldsburg', 'mont-saint-guibert', 'oud-heverlee', 'liedekerke',
+    'chaudfontaine', 'arlon', 'de haan', 'kapellen', 'waasmunster', 'lille',
+    'fléron', 'sprimont', 'damme', 'faimes', 'buggenhout', 'melle', 'herselt',
+    'houthulst', 'ranst', 'gedinne', 'laarne', 'lennik', 'moorslede', 'lendelede',
+    'engis', 'maaseik', 'juprelle', 'tinlot', 'rotselaar', 'ramillies',
+    'sint-pieters-woluwe', 'ichtegem', 'mol', 'mons', 'saint-ghislain',
+    'fosses-la-ville', 'komen-waasten', 'borgloon', 'lokeren', 'dalhem'])
+    province = st.selectbox(label = "What province is the house located in?",index=None, options=['oost-vlaanderen' ,'luik' ,'waals-brabant' ,'antwerpen' ,'brussel',
+ 'vlaams-brabant' ,'henegouwen', 'west-vlaanderen' ,'limburg' ,'luxemburg',
+ 'namen'])
 
     # code for prediction
     price = ''
